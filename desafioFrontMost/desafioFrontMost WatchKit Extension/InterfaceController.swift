77d557fea35980e38c53377c6b01303e5f73481e
Baldissera <guilhermebaldissera@hotmail.com>
2017-09-27 17:25:31 -0300

    Adding jokes!

--- desafioFrontMost/desafioFrontMost WatchKit Extension/InterfaceController.swift
+++ desafioFrontMost/desafioFrontMost WatchKit Extension/InterfaceController.swift
@@ -12,10 +12,11 @@ import Foundation
 
 class InterfaceController: WKInterfaceController {
 
+	var jokes: [Joke]!
+	
     override func awake(withContext context: Any?) {
         super.awake(withContext: context)
-        
-        // Configure interface objects here.
+        self.createJokes()
     }
     
     override func willActivate() {
@@ -28,4 +29,31 @@ class InterfaceController: WKInterfaceController {
         super.didDeactivate()
     }
 
+}
+
+extension InterfaceController {
+	
+	func createJokes() {
+		self.jokes.append(Joke(question: "O que aconteceu com os lápis quando souberam que o dono da Faber Castell morreu?", answer: "Eles ficaram desapontados."))
+		self.jokes.append(Joke(question: "A plantinha foi ao hospital mas não foi atendida, porque?", answer: "Porque só tinha médico de plantão."))
+		self.jokes.append(Joke(question: "Qual foi a primeira vez que os americanos comeram carne?", answer: "Quando chegou Cristóvão ComLombo."))
+		self.jokes.append(Joke(question: "Porque a aranha é o animal mais carente do mundo?", answer: "Porque ela é um aracNeedYou."))
+		self.jokes.append(Joke(question: "O que são quatro pontinhos pretos no leite?", answer: "Formigas."))
+		self.jokes.append(Joke(question: "Por que é fácil ganhar no 'pedra, papel e tesoura' na cracolândia?", answer: "Porque lá eles sempre escolhem pedra."))
+		self.jokes.append(Joke(question: "Para onde vão os homens-bomba depois da morte?", answer: "Para todos os lados."))
+		self.jokes.append(Joke(question: "Por que o anãozinho atravessa a rua correndo?", answer: "Para pegar embalo para subir no meio fio."))
+		self.jokes.append(Joke(question: "O que é um pontinho vermelho em cima do castelo?", answer: "É uma pimenta do reino."))
+		self.jokes.append(Joke(question: "O que é um ponto?", answer: "Um asterisco que passou gel no cabelo."))
+		self.jokes.append(Joke(question: "Tenho 40 laranjas, tiro 20. Quantas laranjas eu tenho?", answer: "Eu sabia isso com maçãs."))
+		self.jokes.append(Joke(question: "Qual é o contrário de salmão?", answer: "Açúcarpé."))
+		self.jokes.append(Joke(question: "Qual é o filme preferido do Batman?", answer: "Os que ele fez."))
+		self.jokes.append(Joke(question: "Qual é o estado brasileiro onde as pessoas mais tomam refrigerante de laranja?", answer: "Fanta Catarina."))
+		self.jokes.append(Joke(question: "Se meia careca tem 84.592 fios de cabelo, uma careca inteira tem quantos?", answer: "Nenhum."))
+		self.jokes.append(Joke(question: "O que acontece quando chove na Inglaterra?", answer: "Vira Inglabarro"))
+		self.jokes.append(Joke(question: "Onde os Minions moram?", answer: "Em Condo-Minions."))
+		self.jokes.append(Joke(question: "Por que a Dilma não sobe em árvore?", answer: "Porque o José Serra."))
+		self.jokes.append(Joke(question: "O que é um pontinho preto no livro?", answer: "Uma formiga querendo entrar pra história."))
+		self.jokes.append(Joke(question: "O que acontece se o Thor assoprar?", answer: "Thornado."))
+		self.jokes.append(Joke(question: "Por que o Thor vai na academia?", answer: "Para ficar Thorneado."))
+	}
 }
---
+++ desafioFrontMost/desafioFrontMost WatchKit Extension/Joke.swift
@@ -0,0 +1,14 @@
+//
+//  Joke.swift
+//  desafioFrontMost WatchKit Extension
+//
+//  Created by Guilherme Baldissera on 27/09/17.
+//  Copyright © 2017 Luis Gustavo Avelino de Lima Jacinto. All rights reserved.
+//
+
+import Foundation
+
+struct Joke {
+	var question: String
+	var answer: String
+}
